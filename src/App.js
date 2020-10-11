60d25938809ee629ccd8eb55c8f0f1ca8f52603a
Shivayan Bora 1022042 <shivayananand.bora@blueyonder.com>
2020-10-11 21:54:34 +0530

    Create Multiple Contexts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import BookList from './components/BookList';
-import Navbar from './components/NavBar';
-import ThemeToggle from './components/ThemeToggle';
-import ThemeContextProvider from './context/ThemeContext';
+import React from "react";
+import BookList from "./components/BookList";
+import Navbar from "./components/NavBar";
+import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./context/AuthContext";
+import ThemeContextProvider from "./context/ThemeContext";
 
 function App() {
   return (
     <div className="App">
       {/* Everytime the state of the context changes, then anything that's consuming the context will get re-rendered with the new value */}
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
